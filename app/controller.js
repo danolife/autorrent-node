d4ab088ba2be4b3b2923d81d617e4dbcac74e7a1
Dany Badr <badr.dany@gmail.com>
2017-03-03 19:28:10 +0100

    Tried to setup OAuth

--- app/controller.js
+++ app/controller.js
@@ -1,26 +1,7 @@
 const config = require('../config/config')
-const tvShowTime = {
-  'client_id': 'QiELLt4riIVJCxDhshZn',
-  //'client_secret': '-UIjzexvB3nLax4pP8-2l6jAj71sCY9KHHUcqBOp'
-  'redirect_uri': config.auth.redirect_uri,
-  'state': 'lkjfsijdfns'
-}
+
 module.exports = {
   index: function() {
     return '<h1>Welcome to Autorrent</h1>'
   },
-  test: function() {
-    return 'test'
-  },
-  auth: function() {
-    return getAuthUrl()
-  }
-}
-
-var getAuthUrl = function() {
-  var url = 'https://www.tvshowtime.com/oauth/authorize'
-  url += '?client_id=' + tvShowTime.client_id
-  url += '&redirect_uri=' + tvShowTime.redirect_uri
-  url += '&state=' + tvShowTime.state
-  return url
 }
--- app/server.js
+++ app/server.js
@@ -2,8 +2,10 @@
 var port = 8080;
 const express = require('express')
 const app = express()
+const config = require('../config/config')
+const passport = require('passport')
+var OAuth2Strategy = require('passport-oauth2').Strategy;
 const controller = require('./controller')
-var tvShowTimeCode;
 
 module.exports = {
   init: function() {
@@ -13,21 +15,38 @@ module.exports = {
     })
 
     app.get('/', (request, response) => {
+      console.log('/')
       response.send(controller.index())
     })
 
-    app.get('/test', (request, response) => {
-      response.send(controller.test())
+    app.get('/auth',
+      passport.authenticate('oauth2'),
+      (request, response) => {
+        console.log('/auth')
+        response.redirect('/')
     })
 
-    app.get('/auth', (request, response) => {
-      response.redirect(controller.auth())
-    })
-
-    app.get('/authback', (request, response) => {
-      tvShowTimeCode = request.query.code
+    app.get('/auth/callback', (request, response) => {
+      console.log('/auth/callback')
       response.redirect('/')
     })
+
+    passport.use(new OAuth2Strategy({
+      authorizationURL: 'https://www.tvshowtime.com/oauth/authorize',
+      tokenURL: 'https://api.tvshowtime.com/v1/oauth/access_token',
+      clientID: 'QiELLt4riIVJCxDhshZn',
+      clientSecret: '-UIjzexvB3nLax4pP8-2l6jAj71sCY9KHHUcqBOp',
+      callbackURL: config.base_url + '/auth/callback'
+    },
+    function(accessToken, refreshToken, profile, cb) {
+      console.log('Access token: ')
+      console.log(accessToken)
+      console.log(refreshToken)
+      console.log(profile)
+      console.log(cb)
+      return cb()
+    }
+    ));
 
     app.use((err, request, response, next) => {
       // log the error, for now just console.log
