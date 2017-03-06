d08124ec94105b768e90c7530eb74eac4f385a62
Dany Badr <badr.dany@gmail.com>
2017-03-06 13:02:11 +0100

    OAuth is working!

--- app/controller.js
+++ app/controller.js
@@ -2,6 +2,6 @@ const config = require('../config/config')
 
 module.exports = {
   index: function() {
-    return '<h1>Welcome to Autorrent</h1>'
+    return '<h1>Welcome to Autorrent</h1><br/><a href="/auth/tvst">Log in</a>'
   },
 }
--- app/server.js
+++ app/server.js
@@ -1,11 +1,12 @@
 #!/usr/bin/env nodejs
-var port = 8080;
+const port = 8080;
 const express = require('express')
 const app = express()
+const controller = require('./controller')
 const config = require('../config/config')
 const passport = require('passport')
-var OAuth2Strategy = require('passport-oauth2').Strategy;
-const controller = require('./controller')
+var OAuth2Strategy = require('passport-oauth2').Strategy
+var token
 
 module.exports = {
   init: function() {
@@ -14,39 +15,68 @@ module.exports = {
       next()
     })
 
-    app.get('/', (request, response) => {
-      console.log('/')
-      response.send(controller.index())
-    })
-
-    app.get('/auth',
-      passport.authenticate('oauth2'),
-      (request, response) => {
-        console.log('/auth')
-        response.redirect('/')
-    })
-
-    app.get('/auth/callback', (request, response) => {
-      console.log('/auth/callback')
-      response.redirect('/')
-    })
-
-    passport.use(new OAuth2Strategy({
+    passport.use('tvst', new OAuth2Strategy({
       authorizationURL: 'https://www.tvshowtime.com/oauth/authorize',
       tokenURL: 'https://api.tvshowtime.com/v1/oauth/access_token',
       clientID: 'QiELLt4riIVJCxDhshZn',
       clientSecret: '-UIjzexvB3nLax4pP8-2l6jAj71sCY9KHHUcqBOp',
-      callbackURL: config.base_url + '/auth/callback'
+      callbackURL: config.base_url + '/auth/tvst/callback'
     },
-    function(accessToken, refreshToken, profile, cb) {
-      console.log('Access token: ')
-      console.log(accessToken)
-      console.log(refreshToken)
-      console.log(profile)
-      console.log(cb)
-      return cb()
+    function(accessToken, refreshToken, profile, done) {
+      token = accessToken
+      done()
     }
-    ));
+  ));
+
+    app.get('/', (request, response) => {
+      console.log('/')
+      if (token) {
+        
+      }
+      response.send(controller.index())
+    })
+
+    // Redirect the user to the OAuth 2.0 provider for authentication.  When
+    // complete, the provider will redirect the user back to the application at
+    //     /auth/provider/callback
+    app.get('/auth/tvst', passport.authenticate('tvst'));
+
+    // The OAuth 2.0 provider has redirected the user back to the application.
+    // Finish the authentication process by attempting to obtain an access
+    // token.  If authorization was granted, the user will be logged in.
+    // Otherwise, authentication has failed.
+    app.get('/auth/tvst/callback',
+      passport.authenticate('tvst', { successRedirect: '/', failureRedirect: '/' })
+    );
+
+    // app.get('/auth',
+    //   passport.authenticate('oauth2'),
+    //   (request, response) => {
+    //     console.log('/auth')
+    //     response.redirect('/')
+    // })
+    //
+    // app.get('/auth/callback', (request, response) => {
+    //   console.log('/auth/callback')
+    //   response.redirect('/')
+    // })
+
+    // passport.use(new OAuth2Strategy({
+    //   authorizationURL: 'https://www.tvshowtime.com/oauth/authorize',
+    //   tokenURL: 'https://api.tvshowtime.com/v1/oauth/access_token',
+    //   clientID: 'QiELLt4riIVJCxDhshZn',
+    //   clientSecret: '-UIjzexvB3nLax4pP8-2l6jAj71sCY9KHHUcqBOp',
+    //   callbackURL: config.base_url + '/auth/callback'
+    // },
+    // function(accessToken, refreshToken, profile, cb) {
+    //   console.log('Access token: ')
+    //   console.log(accessToken)
+    //   console.log(refreshToken)
+    //   console.log(profile)
+    //   console.log(cb)
+    //   return cb()
+    // }
+    // ));
 
     app.use((err, request, response, next) => {
       // log the error, for now just console.log
