0d453a06e6fa20b153629fe4b5ad668bfb945772
Dany Badr <badr.dany@gmail.com>
2017-03-11 12:26:29 +0100

    Revert "Tried to save token client side in cookie"
    This reverts commit bc4aeede1694b1e8f8a6768cfb3a2eb05230890e.

--- app/server.js
+++ app/server.js
@@ -8,12 +8,15 @@ const controller = require('./controller')
 const api = require('./api')
 const config = require('../config/config')
 const passport = require('passport')
-const cookieParser = require('cookie-parser')
 var OAuth2Strategy = require('passport-oauth2').Strategy
 
 module.exports = {
   init: function() {
-    // Templating
+    app.use((request, response, next) => {
+      // console.log(request.headers)
+      next()
+    })
+
     app.engine('.hbs', exphbs({
       defaultLayout: 'layout',
       extname: '.hbs',
@@ -22,25 +25,23 @@ module.exports = {
     app.set('view engine', '.hbs')
     app.set('views', path.join(__dirname, '../views'))
 
-    // Auth strategy
     passport.use('tvst', new OAuth2Strategy({
-        authorizationURL: 'https://www.tvshowtime.com/oauth/authorize',
-        tokenURL: 'https://api.tvshowtime.com/v1/oauth/access_token',
-        clientID: 'QiELLt4riIVJCxDhshZn',
-        clientSecret: '-UIjzexvB3nLax4pP8-2l6jAj71sCY9KHHUcqBOp',
-        callbackURL: config.base_url + '/auth/tvst/callback',
-        passReqToCallback: true
-      },
-      function(req, accessToken, refreshToken, profile, done) {
-        // req.cookie('access_token', accessToken, { maxAge: 900000 })
-        console.log('--token: ' + accessToken)
-        req.headers['tvst_access_token'] = accessToken
-        done()
-      }
-    ));
+      authorizationURL: 'https://www.tvshowtime.com/oauth/authorize',
+      tokenURL: 'https://api.tvshowtime.com/v1/oauth/access_token',
+      clientID: 'QiELLt4riIVJCxDhshZn',
+      clientSecret: '-UIjzexvB3nLax4pP8-2l6jAj71sCY9KHHUcqBOp',
+      callbackURL: config.base_url + '/auth/tvst/callback'
+    },
+    function(accessToken, refreshToken, profile, done) {
+      api.token = accessToken
+      done()
+    }
+  ));
 
-    // Cookies
-    app.use(cookieParser())
+    app.get('/', (req, res) => {
+      console.log('/')
+      controller.index(res)
+    })
 
     // Redirect the user to the OAuth 2.0 provider for authentication.  When
     // complete, the provider will redirect the user back to the application at
@@ -52,40 +53,8 @@ module.exports = {
     // token.  If authorization was granted, the user will be logged in.
     // Otherwise, authentication has failed.
     app.get('/auth/tvst/callback',
-      passport.authenticate('tvst', { successRedirect: '/auth/success', failureRedirect: '/auth/failure' })
+      passport.authenticate('tvst', { successRedirect: '/', failureRedirect: '/' })
     );
-
-    app.get('/auth/success', (req, res) => {
-      console.log('/auth/success')
-      var access_token = req.headers['tvst_access_token']
-      console.log(req.headers['tvst_access_token'])
-      res.cookie('tvst_access_token', access_token, { maxAge: 900000 })
-      res.redirect('/')
-    })
-
-    app.get('/auth/failure', (req, res) => {
-      console.log('/auth/failure')
-      res.cookie('tvst_access_token', 'failure', { maxAge: 900000 })
-      res.redirect('/')
-    })
-
-    app.use(function (req, res, next) {
-      // check if client sent cookie
-      var cookie = req.cookies.access_token;
-      if (cookie === undefined) {
-        console.log('no cookie, redirecting to auth')
-        console.log('url: '+req.url)
-        //return res.redirect('/auth/tvst')
-      } else {
-        console.log('cookie exists', cookie)
-      }
-      next()
-    })
-
-    app.get('/', (req, res) => {
-      console.log('/')
-      controller.index(res)
-    })
 
     app.use((err, request, response, next) => {
       // log the error, for now just console.log
