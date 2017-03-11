6052d7e836b018a3b94cd183cc6b7a105d287057
Dany Badr <badr.dany@gmail.com>
2017-03-11 12:36:39 +0100

    Actual request to get user (evrytime)

--- app/api.js
+++ app/api.js
@@ -1,21 +1,15 @@
 const request = require('request')
 
 module.exports = {
-  user: undefined,
   getUser(token, cb) {
-    if (module.exports.user == undefined) {
-      request({
-        url: 'https://api.tvshowtime.com/v1/user',
-        qs: {access_token: token}
-      }, function(err, response, body) {
-        module.exports.user = JSON.parse(body);
-        cb(module.exports.user)
-        return module.exports.user
-      })
-    } else {
-      cb(module.exports.user)
-      return module.exports.user
-    }
+    request({
+      url: 'https://api.tvshowtime.com/v1/user',
+      qs: {access_token: token}
+    }, function(err, response, body) {
+      var user = JSON.parse(body);
+      cb(user)
+      return user
+    })
   },
   getWatchList: function(token, cb) {
     request({
